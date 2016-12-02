eb527e7f91a5fdbeace64e811a1b7bf05c09ac03
erikm0111 <erikmatosevic@gmail.com>
2016-12-02 19:41:41 +0100

    Second part of day two

---
+++ day2/day2b.py
@@ -0,0 +1,31 @@
+def moveIfPossible(letter, x, y):
+	if letter == "U":
+		if y > 0 and grid[y-1][x] != "*":
+			y -= 1
+	elif letter == "D":
+		if y < 4 and grid[y+1][x] != "*":
+			y += 1
+	elif letter == "L":
+		if x > 0 and grid[y][x-1] != "*":
+			x -= 1
+	else:
+		if x < 4 and grid[y][x+1] != "*":
+			x += 1
+	return x, y
+
+
+content = []
+with open("input.txt") as f:
+	content = f.readlines()
+
+grid = [["*","*","1","*","*"],
+		["*","2","3","4","*"],
+		["5","6","7","8","9"],
+		["*","A","B","C","*"],
+		["*","*","D","*","*"]]
+
+x, y = 0, 2
+for line in content:
+	for letter in line.strip():
+		x,y = moveIfPossible(letter, x, y)
+	print grid[y][x],
